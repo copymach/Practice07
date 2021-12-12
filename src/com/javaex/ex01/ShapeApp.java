6c708a84203b840e6407908e99fd01ad5d8c96bb
copymachine <copymach@naver.com>
2021-12-12 21:42:00 +0900

    붕어빵3000원

--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,13 +4,15 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-//		Shape s = new Shape ("빨강");
+//		아래코드 정상화 방법을 모르겠다..
+//		Shape s = new Shape ("빨강"); 
 		Circle c1 = new Circle("녹색", 10);
 		Rectangle r1 = new Rectangle("빨강", 4, 4);
 		Shape sr1 = new Rectangle("빨강", 4, 4);
 		
 		//sr1을 그리는 메소드 호출
 		sr1.draw();
+		
 		//sr1의 가로크기 출력
 		((Rectangle)sr1).getWidth();
 		System.out.println("sr1의 가로는 "+((Rectangle)sr1).getWidth());
--- src/com/javaex/ex03/Soundable.java
+++ src/com/javaex/ex03/Soundable.java
@@ -2,7 +2,7 @@ package com.javaex.ex03;
 
 public interface Soundable {
 	
-//	변경하지 말것
+//	추상클래스 변경하지 말것
 	
 	public String sound(); 
 	
